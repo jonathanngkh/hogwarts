4a7bad3cf3c559bc1331a39a1abab9096536195e
Jonathan Ng <jonngkh@hotmail.com>
2016-04-09 16:58:57 +0800

    rspec added

---
+++ lib/intro.rb
@@ -0,0 +1,113 @@
+class Text_Master
+  def text_break(content)
+    content.split
+  end
+end
+
+puts "Hi, I'm Professor Jonngkh. ▼"
+
+gets.chomp
+
+puts "Welcome to the world of witchcraft and wizardry! ▼"
+
+gets.chomp
+
+puts "This - is magic. ▼"
+
+gets.chomp
+
+puts "Lumos!"
+
+✨
+
+puts "What's your name?"
+
+name = gets.chomp
+
+puts "Are you a boy or a girl? (boy/girl/???)"
+
+gender = gets.chomp
+
+if gender == "boy"
+  puts "You're a wizard, #{name}!"
+  gender = "wizard"
+elsif gender == "girl"
+  puts "You're a witch, #{name}!"
+  gender = "witch"
+else
+  puts "You're queer, #{name}! The most magical being of all! ▼"
+
+  gets.chomp
+
+  puts "Don't worry, you can be yourself here. ▼"
+
+  gets.chomp
+
+  puts "Only muggles get all caught up in things like gender and don't see you for who you want to be. ▼"
+
+  gender = "magical being"
+end
+
+puts "Do you believe me? (yes/no)"
+
+belief = gets.chomp
+
+if belief == "yes"
+  puts "As I thought! You've always known it, haven't you? ▼"
+else
+  puts "Trust me. You've always known it. ▼"
+end
+
+gets.chomp
+
+puts "If you've ever felt the warmth of a parent's hug, ▼"
+
+gets.chomp
+
+puts "the flutter of your heart walking past a complete stranger, ▼"
+
+gets.chomp
+
+puts "or benevolence welling up inside when you see a helpless animal, ▼"
+
+gets.chomp
+
+puts "Then you've known love - The most powerful form of magic. ▼"
+
+gets.chomp
+
+puts "#{name}, you have the potential to be a great #{gender} ▼"
+
+puts "I have so much to tell you. ▼"
+
+gets.chomp
+
+puts "But first, what say you we have some tea? Would you like something to eat or drink? (eat/drink/nope)"
+
+tea = gets.chomp
+
+if tea == "eat"
+  puts "Where did I put those chocolate frogs again?"
+
+  gets.chomp
+
+  puts "Accio chocolate frogs!"
+
+  gets.chomp
+
+  puts "Jonngkh.accio(chocolate_frogs)"
+
+  gets.chomp
+
+  puts "*ribbit*"
+
+  gets.chomp
+
+elsif tea = "drink"
+  puts ""
+end
+
+puts "I love the world. The muggle world, the wizarding world, the animal world, the spiritual world. That's part of the reason why I'm here, to study the digital world! The other part of the reason..."
+
+puts "I'll explain another time."
+
